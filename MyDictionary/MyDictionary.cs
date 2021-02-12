40bf22f6c06d3fb3603873977eafd5ad272cf3ec
Kumriye Yıldız <kumryldz@gmail.com>
2021-02-12 23:02:52 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyList<K>
+    {
+        K[] units;
+
+
+        public MyList()
+        {
+            units = new K[0];
+        }
+
+
+        public void Add(K unit)
+        {
+            K[] tempArray = units;
+            units = new K[units.Length + 1];
+
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                units[i] = tempArray[i];
+            }
+
+            units[units.Length - 1] = unit;
+        }
+
+
+        public K[] Units
+        {
+            get { return units; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> unit = new MyList<string>();
+            unit.Add("Mathematics");
+            unit.Add("Book");
+            unit.Add("Pencil");
+
+
+            foreach (var items in unit.Units)
+            {
+                Console.WriteLine(items);
+            }
+
+        }
+    }
+}
